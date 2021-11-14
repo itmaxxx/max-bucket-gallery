ad57dc97f7b2199e352c7e7cffa76cbdd118b778
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-14 21:29:50 +0200

    Refactored user router

--- apps/api/src/routes/userRouter.ts
+++ apps/api/src/routes/userRouter.ts
@@ -1,22 +1,10 @@
 import { Router } from 'express';
 import { isAuthorized } from '../middlewares/isAuthorized';
-import { hasSameId } from '../middlewares/hasSameId';
-import { RequestWithUser } from '../types';
 import UsersController from '../controllers/usersController';
 
 const router = Router();
 const usersController = new UsersController();
 
-router.get('/me', isAuthorized, async (req: RequestWithUser, res) => {
-  await usersController.getCurrentUser(req, res);
-});
-
-router.get('/:userId', isAuthorized, async (req, res) => {
-  // await getUserInfo(req, res);
-});
-
-router.patch('/:userId', isAuthorized, hasSameId, async (req, res) => {
-  // await editUserInfo(req, res);
-});
+router.get('/me', isAuthorized, usersController.getCurrentUser);
 
 export default router;
