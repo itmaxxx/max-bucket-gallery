4e548fefe4b6bb2072ad0259ec1eaf704a8206d3
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-28 13:28:51 +0200

    Remove unused routes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import passport from 'passport';
-import { Message } from '@max-bucket-gallery/api-interfaces';
-import { isAuthorized } from './middlewares/isAuthorized';
 import authRouter from './routes/authRouter';
 import userRouter from './routes/userRouter';
 import imagesRouter from './routes/imagesRouter';
@@ -17,7 +15,6 @@ const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } =
 
 try {
   const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-  // console.log(url);
   mongoose.connect(url).then(async () => {
     console.log('MongoDB is connected');
   });
@@ -37,19 +34,10 @@ setUpGoogleStrategy();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/images', formidableMiddleware());
+
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/images', imagesRouter);
-
-app.get('/api', (req, res) => {
-  return res.send({ message: 'Hello' });
-});
-
-app.get('/api/private', isAuthorized, (req, res) => {
-  const msg: Message = { message: 'You are authorized' };
-
-  return res.send(msg);
-});
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
